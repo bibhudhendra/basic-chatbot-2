f0564e0e453da481e3d4441b6322bf1b1f014751
Bibhu Pati <bibhudh@amazon.com>
2024-09-29 11:06:10 +0530

    Update styling

--- src/utils/renderers.js
+++ src/utils/renderers.js
@@ -1,13 +1,19 @@
 // src/utils/renderers.js
 
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { darcula } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { prism } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 export const renderers = {
   code({ node, inline, className, children, ...props }) {
     const match = /language-(\w+)/.exec(className || '');
+
     return !inline && match ? (
-      <SyntaxHighlighter style={darcula} language={match[1]} PreTag="div" {...props}>
+      <SyntaxHighlighter
+        style={prism} // Use the light theme here
+        language={match[1]}
+        PreTag="div"
+        {...props}
+      >
         {String(children).replace(/\n$/, '')}
       </SyntaxHighlighter>
     ) : (
